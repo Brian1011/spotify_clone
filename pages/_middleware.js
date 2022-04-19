67129ac6f4149f7e30447ec7948d61d3df255af3
Brian Mutinda <mutindabrian26@gmail.com>
2022-04-19 22:08:48 +0300

    capture request url

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,13 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+    // token will exists if user is logged in
     const token = await getToken({req, secret:process.env.JWT_SECRET});
+
+    // capture the next url
+    const {pathname} = req.nextUrl;
+
+    
 }
 
 // middleware
